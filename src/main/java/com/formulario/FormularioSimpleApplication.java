330894acf1c444fd73d025de3a484751810f17ff
AJDeLavalle <96790352+Antomaker@users.noreply.github.com>
2025-11-19 12:11:16 -0500

    carga base

---
+++ src/main/java/com/formulario/FormularioSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.formulario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularioSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormularioSimpleApplication.class, args);
+	}
+
+}
